852077b2c05bafde4ea5d6a6223623d461d599b9
Riley Benson <jordan.riley.benson@gmail.com>
2023-11-30 13:59:30 -0500

    Updated with relative base to handle redirect.

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: './',
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
-        nimbus: resolve(__dirname, "nimbus/index.html"),
+        main: resolve(__dirname, 'index.html'),
+        nimbus: resolve(__dirname, 'nimbus/index.html'),
       },
     },
   },
-});
+})
